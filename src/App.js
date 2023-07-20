3699f6cc400d42af5f16094aa696a151839e6f59
fabiotano <fabio.capriotti20@gmail.com>
2023-07-20 19:53:57 +0200

    creacion fetch URL

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const  {people, setPeople} = useState([]);
-  const  {planets, setPlanets} = useState([]);
-  const  {loading, setLoading} = useState([true])
+  const [ people, setPeople ] = useState([]);
+  const [ planets, setPlanets ] = useState([]);
+  const [ loading, setLoading ] = useState([true])
+
+  useEffect(() => {
+
+    async function fetchPeople() {
+      let res = await fetch('https://swapi.dev/api/people/?format=json')
+      let data = await res.json()
+      setPeople(data.results)
+    }
+
+    async function fetchPlanets() {
+      let res = await fetch('https://swapi.dev/api/planets/?format=json')
+      let data = await res.json()
+      setPlanets(data.results)
+    }
+
+    fetchPeople();
+    fetchPlanets();
+  })
+
+  console.log('people', people)
+  console.log('people', planets)
 
   return (
     <div className="App">
